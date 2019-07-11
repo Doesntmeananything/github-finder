b130a6ab85a4ce2d09adb45fbd058ebf88911b79
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-11 23:01:56 +0300

    Add better styling

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const Navbar = ({ icon, title }) => {
   return (
-    <nav className="bg-primary">
+    <nav className="navbar bg-primary">
       <h1>
         <i className={icon} /> {title}
       </h1>
