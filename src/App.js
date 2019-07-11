5df3a7f4784351bf63158acca29ccc8a5c864a7c
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-11 20:29:39 +0300

    Add search request

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import axios from "axios";
 import "./App.css";
+import PropTypes from "prop-types";
 
 class App extends Component {
   state = {
@@ -11,24 +12,27 @@ class App extends Component {
     loading: false
   };
 
-  async componentDidMount() {
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired
+  };
+
+  searchUsers = async text => {
     this.setState({ loading: true });
 
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${
+      `https://api.github.com/search/users?q=${text}&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: res.data, loading: false });
-    console.log(res.data);
-  }
+    this.setState({ users: res.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
