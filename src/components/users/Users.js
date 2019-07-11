920aac1672d47b1fff2277fa1cedd2f3365ce515
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-11 19:49:42 +0300

    Add Spinner component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,22 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import UserItem from "./UserItem";
-
-export class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
-  }
-}
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1rem"
+};
+
+const Users = ({ users, loading }) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map(user => (
+          <UserItem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+Users.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 };
 
 export default Users;
