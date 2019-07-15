d1a5ce88fc452ca1d12f93b5d6a389d690a4f5ec
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-07-15 17:37:01 +0300

    Add repos to UI

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
-import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import Repos from "../repos/Repos";
 
 export class User extends Component {
   componentDidMount() {
     this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
   }
 
   static propTypes = {
     loading: PropTypes.bool,
     user: PropTypes.object.isRequired,
-    getUser: PropTypes.func.isRequired
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired
   };
 
   render() {
     const {
       name,
       avatar_url,
-      url,
       location,
       bio,
       blog,
@@ -32,7 +35,7 @@ export class User extends Component {
       company
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -99,6 +102,7 @@ export class User extends Component {
           <div className="badge badge-light">Public Repos: {public_repos}</div>
           <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
+        <Repos repos={repos} />
       </>
     );
   }
